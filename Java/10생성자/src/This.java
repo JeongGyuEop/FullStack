7dc5a0729c9c3f0b8759973505f1a9fbcaa672c7
JeongGyuEop <gueeop0219@naver.com>
2024-08-31 18:52:34 +0900

    FullStack Java 생성자 & this HomeWork

--- Java/10생성자/src/This.java
+++ Java/10생성자/src/This.java
@@ -1,8 +1,41 @@
 
+
+/*
+ * 	this 키워드
+ * 		- This class 설계도 내부에서 this를 작성하면
+ * 		  this는 현재 생성한 객체 자신을 가리키는 참조변수
+ * 
+ * 		  예) new This(); --> 0x16 객체 메모리의 주소번지
+ * 
+ * 		- 사용 목적
+ * 			- 객체변수와 매개변수명이 같을때 구분하기 위해서 this.객체변수명 = 매개변수명;
+ * 				this. 을 작성하여 객체 메모리의 객체 변수를 가리킬 때 사용
+			
+			-  생성된 객체의 메소드나 변수에 접근 :
+				객체 자신의 메소드나 변수에 접근할 때 this를 사용할 수 있다.
+ * 
+ */
+
+class Car2 {
+	String color;
+	int year;
+	// 생성자 - 두개의 객체 변수를 모두 초기화할 생성자
+	// 단축키 alt + shift + s o
+	// 생성자 만들때 메뉴 순서
+	// Source 메뉴 클릭 -> 
+	
+	
+	
+	
+} 
+
+
+
 public class This {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
 
 	}
 
