5ecff2cc7e2cce5e11e3f3feeb714271585324d3
GYUEOP <gueeop0219@naver.com>
2024-09-26 09:20:53 +0900

    FullStack Java 스레드 24/09/26

--- Java/25스레드/src/ThreadEx03.java
+++ Java/25스레드/src/ThreadEx03.java
@@ -1,16 +1,20 @@
 
 
 //보조작업 스레드 객체를 생성하기 위한 사용자정의 스레드 클래스 만들기
-//방법1. Thread클래스를 상속받아 ThreadDemo라는 이름으로 스레드 클래스 만들기
-class ThreadDemo extends Thread{
+//방법2. Runnable인터페이스를 구현받아 run추상메소드를 가진 일반클래스로 만들자
+class ThreadDemo1 implements Runnable{
+	
 	//숫자를 카운팅해서 저장할 int count변수선언
 	int count;
-	//보조작업스레드(ThreadDemo)객체 생성시 
+	//스레드 이름을 저장할 변수 선언
+	String name;
+		
+	//일반클래스(ThreadDemo1)객체 생성시 
 	//스레드이름을 매개변수로 받아 초기화할 생성자 선언
 	//단! 생성자 내부에서 run메소드가 실행되게 start()메소드를 호출해 주세요
-	public ThreadDemo(String name) {
-		super(name);
-		this.start();
+	public ThreadDemo1(String name) { //"보조작업스레드객체1"
+		this.name = name;
+		new Thread(this, name).start();
 	}
 	
 	//보조작업스레드가 하는작업을 run메소드를 오버라이딩해서 선언
@@ -18,7 +22,7 @@ class ThreadDemo extends Thread{
 	public void run() {
 	//재구현 코드 내용
 		//현재 실행되고 있는 보조작업스레드객체의 이름을 얻어 일 "시작"  출력
-		System.out.println(this.getName() + "  시작");
+		System.out.println(this.name + "  시작");
 		// 0 ~ 9 까지 1씩 증가 시키면서 1초 간격으로 숫자를 반복해서 출력하는일
 		do {
 			//현재 작업중인 보조스레드객체에게 1초 휴식
@@ -29,18 +33,18 @@ class ThreadDemo extends Thread{
 			}
 			
 			//현재 보조작업스레드가 run메소드를 실행하고 있으며 스레드이름을 받아 같이 출력
-			System.out.println(this.getName() + ", 카운터 = " + this.count);
+			System.out.println(this.name + ", 카운터 = " + this.count);
 			
 			count++;
 		}while(count<10);
 
 
 		//현재 실행되고 있는 보조작업스레드객체의 이름을 얻어 일 "끝"  출력
-		System.out.println(this.getName() + "  끝");
+		System.out.println(this.name + "  끝");
 	}
 		
 }			
-public class ThreadEx02 {
+public class ThreadEx03 {
 
 	//주(메인)스레드
 	//하는일 : 보조작업스레드 객체 생성하는일,  
@@ -48,8 +52,9 @@ public class ThreadEx02 {
 	public static void main(String[] args) throws InterruptedException {		
 		System.out.println("메인스레드 일 시작");
 		
-		//보조작업스레드 객체 생성
-		new ThreadDemo("보조작업스레드객체1");
+		//보조작업스레드 객체를 만들기위한 run추상메소드를 오버라이딩 해놓은 일반클래스의 객체 생성
+		//객체 생성시 보조작업스레드 이름을 생성자로 전달해 초기화 시킴
+		new ThreadDemo1("보조작업스레드객체1");
 		
 		//0.5초 간격으로 .을 10번 반복해서 출력하는일
 		int count = 0;
